4884cd6a5dd9084b71317c1b718a0bbfd89bc259
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-03-14 20:05:50 -0700

    add clone of shadi.com

---
+++ Extra-Practice/shadi.com/index.js
@@ -0,0 +1,32 @@
+let items = document.querySelectorAll('#recipeCarousel .carousel-item')
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+let items1 = document.querySelectorAll('#recipeCarousel-1 .carousel-item-1')
+
+items1.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items1[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
