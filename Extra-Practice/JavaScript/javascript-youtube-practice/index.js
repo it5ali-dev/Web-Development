64e3f023fe586ae4faf24649a4a1b53dab9ffdc1
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-05-01 22:13:48 +0500

    play around with types, type casting, arithmetic operators

--- Extra-Practice/JavaScript/javascript-youtube-practice/index.js
+++ Extra-Practice/JavaScript/javascript-youtube-practice/index.js
@@ -5,17 +5,28 @@
 // window.alert("i like pizza")
 
 
-
+// ==========================================
 // variables
+/*
+// Strings
 let fname = 'Ali';
 let lname = 'Hassan';
-let age = 22;
-let price = 12.99;
-let gpa = 3.33;
 let food = 'Burger';
 let email = 'alihassan@gmail.com';
 
+// Numbers
+let age = 22;
+let price = 12.99;
+let gpa = 3.33;
 
+// Boolean
+let online = 'true';
+let forsale = 'true';
+let student = 'true';
+
+
+console.log(`this car is for sale: ${forsale}`);
+console.log(`${fname} is online: ${online}`);
 console.log(`your name is ${fname}`);
 console.log(`you are ${age} years old`);
 console.log(`your gpa is ${gpa}`);
@@ -25,8 +36,70 @@ console.log(typeof(age));
 console.log(typeof(fname));
 console.log(typeof(gpa));
 
-
-
 document.getElementById('myhead1').textContent = `Hello ${fname + " " + lname}`;
 document.getElementById('mypara1').textContent = `i like ${food}`;
 document.getElementById('mypara2').textContent = `your email is: ${email}`;
+
+*/
+// ============================================
+
+// ============================================
+// Arithmatic Operator
+// let student = 30;
+// student++; // student = student + 1 | student += 1 (Augmented Assignment Operator)
+// student--; // student = student - 1 | student -= 1
+// student *= 2; // student = student * 2
+// student /= 2; // student = student / 2
+// student **= 2 // student = student ** 2 (means power of 2)
+
+
+// console.log(student);
+
+// ============================================
+
+// ============================================
+
+// How to get input from the user
+// two ways of getting input from user
+
+// let username;
+
+// 1. using window.prompt method
+
+// username = window.prompt("Enter your name? ");
+// console.log(`your username: ${username}`);
+
+// 2. By using input element and tag that using js to get the input data
+
+// document.getElementById('mybutton').onclick = function() {
+//     username = document.getElementById('text').value;
+//     document.getElementById('myhead1').textContent = `Hello ${username}`
+// }
+
+
+// ============================================
+
+// ============================================
+
+// type conversion
+// let age = window.prompt("What's your age? ");
+// age = Number(age); // convert String to number
+// age += 1;
+// console.log(`you are ${age} years old: `, typeof(age));
+
+let x = 'pizza';
+let y = 'pizza';
+let z = 'pizza';
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof(x));
+console.log(y, typeof(y));
+console.log(z, typeof(z));
+
+
+
+// ============================================
+
