bc3a791580e1a0181586387041b146747c2d1a00
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-05-11 22:54:07 +0500

    play around with function expression

--- Extra-Practice/JavaScript/youtube/functionsJS/index.js
+++ Extra-Practice/JavaScript/youtube/functionsJS/index.js
@@ -37,5 +37,39 @@
 // console.log(`email is valid: ${isValidEmail('ali@fake.com')}`);
 // console.log(`email is valid: ${isValidEmail('alifake.com')}`);
 
+// ========================================
+// function Expression
+// const hello = function() {
+//     console.log('Hello');
+// }
+
+// setTimeout(hello, 3000);
+
+// setTimeout(() => {
+//     console.log('World');
+// }, 3000);
+
+// =======================================
+
+// const number = [1,2,3,4,5]
+// const numSquare = number.map(square)
+
+// console.log(numSquare);
+
+// function square(element) {
+//     return Math.pow(element, 2)
+// }
+
+// Alternate of above
+const number = [1, 2, 3, 4, 5, 6]
+const numSquare = number.map(function (element) {
+    return Math.pow(element, 2)
+})
+
+const isEven = number.filter(function(e) {
+    return e%2 === 0
+})
+
+console.log(isEven);
 
 
