d70d74b7927c591e74bbd53b12326c18dbbeed6a
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-05-16 23:38:17 +0500

    project 3 using EJS to show the list of feedback

---
+++ Extra-Practice/projects/8.3D/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Import JSON data
+const jsonIcecreamTypeData = require('./data/icecreamtypes.json');
+const jsonCommentData = require('./data/comments.json');
+const jsonUserData = require('./data/users.json');
+
+// Set up EJS template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Route handlers
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Ice Cream Review' });
+});
+
+app.get('/users', (req, res) => {
+    res.json(jsonUserData);
+});
+
+app.get('/icecreamtypes', (req, res) => {
+    res.json(jsonIcecreamTypeData);
+});
+
+app.get('/comments', (req, res) => {
+    res.json(jsonCommentData);
+});
+
+app.get('/feedback', (req, res) => {
+    res.render('list', {
+        title: 'User Feedback',
+        types: jsonIcecreamTypeData.icecreamTypes,
+        comments: jsonCommentData.comments, // Corrected property name from `comment` to `comments`
+        users: jsonUserData.users
+    });
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
