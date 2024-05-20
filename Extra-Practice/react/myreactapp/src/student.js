92e3efa505ede57626357033736be0067773ed4b
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-05-20 17:38:16 +0500

    intro to react

---
+++ Extra-Practice/react/myreactapp/src/student.js
@@ -0,0 +1,11 @@
+// Must start with Capital letter not small (Its Mandatory)
+const Student = ({name, address}) => {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{address}</p>
+        </div>
+    )
+}
+
+export default Student
