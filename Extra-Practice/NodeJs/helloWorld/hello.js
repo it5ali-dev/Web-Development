938e4e122a9a94aa40ba5ff4f507d329dc8cbdb9
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-05-27 07:44:24 +0500

    getting started with Node

---
+++ Extra-Practice/NodeJs/helloWorld/hello.js
@@ -0,0 +1,15 @@
+const math = require("./math")
+// destructuring 
+// const {add, sub} = require("./math")
+
+console.log(math);
+// console.log(`Math value is: ${math(2, 5)}`);
+
+// console.log(`Math value is: ${math.add(2, 4)}`);
+// console.log(`Math value is: ${math.sub(2, 4)}`);
+
+// console.log(`Math value is: ${math.addFun(2, 4)}`);
+// console.log(`Math value is: ${math.subFun(2, 4)}`);
+
+// console.log(`Math value is: ${add(2, 4)}`);
+// console.log(`Math value is: ${sub(2, 4)}`);
